f7a35793c979ed4ccdb152fd57272ba84a114466
Victor Andrade <vmfae@iscte.pt>
2015-04-01 21:06:08 +0100

    Teste no mac

--- src/view/MainTest.java
+++ src/view/MainTest.java
@@ -13,6 +13,8 @@ public class MainTest {
 	/**
 	 * Launch the application.
 	 */
+		
+		//MAc...
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
