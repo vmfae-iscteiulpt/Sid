8afd81edafe4e79549e2df2d99122b3c2b3bc9d9
vmfae <vmfae@iscte.pt>
2015-04-03 12:02:55 +0100

    update v1.9

--- src/controller/DbConnection.java
+++ src/controller/DbConnection.java
@@ -23,49 +23,33 @@ public class DbConnection {
 		String dbUrl = "jdbc:sqlanywhere:Tds:localhost:2638?eng=dbGroup9";
 		try {
 			conn = DriverManager.getConnection(dbUrl, "sid9", "sql9");
-			
-			
-			
+						
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			System.err.println("erro ao conectar a base de dados...");
 			e.printStackTrace();
 		}
-		
-		
+	}
+	
+	
+	public Connection getConn() {
+		return conn;
 	}
 
-	public static void main(String[] args) {
-		DbConnection db=new  DbConnection();
-		System.out.println(db.getListDificuldade());
-	}
-	
-	
-	public ArrayList<Docente> getListDocentes() {
-		ArrayList<Docente> lista = new ArrayList<Docente>();
+	public ResultSet select (String query){
 		try {
-			statement = conn.prepareStatement("SELECT * FROM Docente");
+			statement = conn.prepareStatement(query);
 			resultSet = statement.executeQuery();
-			while(resultSet.next()) {
-				String email = resultSet.getString("Email_Docente");
-				String nome = resultSet.getString("Nome");
-				String password = resultSet.getString("Senha");
-				Docente docente = new Docente(email, nome, password);
-				lista.add(docente);
-				
-			}
-			System.out.println("DB DONCENTE close?  "+conn.isClosed());
-			conn.close();
-			System.out.println("DB DONCENTE close? "+conn.isClosed());
-
+			
 		} catch (SQLException e) {
-			System.err.println("problemas na ligação a base de dados, por favor tente novemente!");
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		return lista;
+		return resultSet;
 	}
 	
-
+	
+//LIXO
 	public ArrayList<Difficulty> getListDificuldade() {
 		ArrayList<Difficulty> listaDificuldade = new ArrayList<Difficulty>();
 		
@@ -78,7 +62,6 @@ public class DbConnection {
 				Difficulty d = new Difficulty(designacaoNivel);
 				listaDificuldade.add(d);
 //				System.out.println(d.getDesignacaoNivel());
-
 
 			}	
 
